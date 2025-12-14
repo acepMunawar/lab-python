6ce01dcc1935b8ae7232e59e2a97847c4cc07c42
acepMunawar <acepmunawar96@gmail.com>
2025-12-14 20:16:32 +0700

    feat: update

--- oop/inheritance/abstract-base-class/shape.py
+++ oop/inheritance/abstract-base-class/shape.py
@@ -5,7 +5,4 @@ class Shape(ABC):
     def area(self):
         pass
 
-class Rectangle(Shape):
-    def __init__(self, length, widht):
-        self.length = length
-        self.widht = widht
+    
